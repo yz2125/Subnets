324f19e9527a512f93fb24d04f33e3bf7009a022
yz2125 <157758526+yz2125@users.noreply.github.com>
2024-03-25 20:15:30 -0400

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 
+
+#problem 1
+p1Network = ''
+p1Broadcast = ''
+p1First = ''
+p1Last =  ''
+
+#problem 2
+p2Network = ''
+p2Broadcast = ''
+p2First = ''
+p2Last = ''
+
+#problem 3
+p3Network = ''
+p3Broadcast = ''
+p3First = ''
+p3Last = ''
+
+#problem 4
+p4Network = ''
+p4Broadcast = ''
+p4First = ''
+p4Last = ''
+
+#problem 5
+p5Network = ''
+p5Broadcast = ''
+p5First = ''
+p5Last = ''
+
+#problem 6
+p6Network = ''
+p6Broadcast = ''
+p6First = ''
+p6Last =  ''
+
+#problem 7
+p7Network = ''
+p7Broadcast = ''
+p7First = ''
+p7Last = ''
+
+#problem 8
+p8Network = ''
+p8Broadcast = ''
+p8First = ''
+p8Last = ''
+
+#problem 9
+p9Network = ''
+p9Broadcast = ''
+p9First = ''
+p9Last = ''
+
+#problem 10
+p10Network = ''
+p10Broadcast = ''
+p10First = ''
+p10Last = ''
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
